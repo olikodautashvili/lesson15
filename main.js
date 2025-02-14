74e252cfaf6f2a7f61aa70f1c41a558c2f2b44b5
olikodautashvili <olikodautashvili@gmail.com>
2025-02-15 00:52:11 +0400

    javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let name= "oliko";
+let dayOfWeek = "friday";
+let favoriteActivity="reading";
+
+let str = "Hi, my name is " + name + " Today is " + dayOfWeek + ", a perfect day for "+ favoriteActivity +".";
+
+console.log(str);
+
+let x= 3
+let y=5
+let z= x+y
+
+let number="5 + 3 = "+z;
+
+console.log(number)
